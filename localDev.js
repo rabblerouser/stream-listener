1fc9faacce2582c2c3cec4bc98cd8fcb915a6c2c
Cam Jackson <camjackson89@gmail.com>
2017-02-12 23:11:58 +1100

    Dockerise the event-forwarder for use with compose

--- localDev.js
+++ localDev.js
@@ -2,7 +2,7 @@ const lambda = require('.').handler;
 const AWS = require('aws-sdk')
 
 const kinesis = new AWS.Kinesis({
-  endpoint: 'http://localhost:4567',
+  endpoint: process.env.KINESIS_URL,
   region: 'ap-southeast-2',
   accessKeyId: 'FAKE',
   secretAccessKey: 'ALSO FAKE',
@@ -20,26 +20,31 @@ const mapKinesisRecord = record => ({
 const fetchAndProcessRecords = (shardIterator) => {
   return kinesis.getRecords({ ShardIterator: shardIterator }).promise().then((records) => {
     records.Records.forEach(kinesisRecord => {
+      console.log('Got record from stream:', kinesisRecord);
       const event = { Records: [{ kinesis: mapKinesisRecord(kinesisRecord) }] };
       lambda(event, null, callback);
     });
     setTimeout(() => fetchAndProcessRecords(records.NextShardIterator), 500);
   });
 }
+const main = () => {
+  return kinesis.describeStream({ StreamName: 'rabblerouser_stream' }).promise()
+    .then(stream => {
+      console.log('Found rabblerouser_stream!');
+      const shardId = stream.StreamDescription.Shards[0].ShardId
 
-kinesis.describeStream({ StreamName: 'rabblerouser_stream' }).promise()
-  .then(stream => {
-    console.log('Found rabblerouser_stream...');
-    const shardId = stream.StreamDescription.Shards[0].ShardId
-
-    const params = { ShardId: shardId, ShardIteratorType: 'LATEST', StreamName: 'rabblerouser_stream' };
-    return kinesis.getShardIterator(params).promise();
-  })
-  .then(shardIterator => {
-    console.log('Polling kinesis for events...');
-    return shardIterator.ShardIterator;
-  })
-  .then(fetchAndProcessRecords)
-  .catch(err => {
-    console.error(err);
-  });
+      const params = { ShardId: shardId, ShardIteratorType: 'LATEST', StreamName: 'rabblerouser_stream' };
+      return kinesis.getShardIterator(params).promise();
+    })
+    .then(shardIterator => {
+      console.log('Polling kinesis for events...');
+      return shardIterator.ShardIterator;
+    })
+    .then(fetchAndProcessRecords)
+    .catch(err => {
+      console.error(err.message);
+      console.log('Restarting...');
+      setTimeout(main, 2000);
+    });
+};
+main();
