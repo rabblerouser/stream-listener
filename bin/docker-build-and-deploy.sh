016fb32515661eb9ea926ad86f58f54d0efddcf2
phazel <cwebster@thoughtworks.com>
2017-07-03 12:38:38 +1000

    Pull docker image when building

--- bin/docker-build-and-deploy.sh
+++ bin/docker-build-and-deploy.sh
@@ -3,5 +3,5 @@ set -e
 
 docker login -u "$DOCKER_USER" -p "$DOCKER_PASSWORD"
 
-docker build -t rabblerouser/event-forwarder .
+docker build --pull -t rabblerouser/event-forwarder .
 docker push rabblerouser/event-forwarder
