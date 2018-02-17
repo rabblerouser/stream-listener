b45ca7c10167712f552cc18e44009c072ff09435
Cam Jackson <camjackson89@gmail.com>
2018-02-17 13:03:52 +0800

    Add more logging

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ const handler = require('./src');
 
 const endpoint = process.env.LISTENER_ENDPOINT;
 const authToken = process.env.LISTENER_AUTH_TOKEN;
-exports.handler = handler(request, endpoint, authToken);
+exports.handler = handler(request, endpoint, authToken, console);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 'use strict';
 
-module.exports = (request, uri, auth) => (event, context, callback) => {
-  const promises = event.Records.map(record => {
+module.exports = (request, uri, auth, console) => (event, context, callback) => {
+  console.log(`event-forwarder lambda received ${event.Records.length} event(s)`);
+  const promises = event.Records.map((record, index) => {
+    console.log(`Forwarding event ${index}`);
+    console.log(`    partitionKey: ${record.kinesis.partitionKey}`);
+    console.log(`    sequenceNumber: ${record.kinesis.sequenceNumber}`);
     const options = {
       method: 'POST',
       uri,
@@ -10,7 +14,12 @@ module.exports = (request, uri, auth) => (event, context, callback) => {
       headers: { Authorization: auth },
     };
 
-    return request(options);
+    return request(options)
+      .then(() => console.log(`Received success response from API for event ${record.kinesis.sequenceNumber}`))
+      .catch((e) => {
+        console.error(`Error sending event to API. Event: ${record.kinesis.sequenceNumber}, Error: ${e}`);
+        throw e;
+      });
   });
 
   return Promise.all(promises).then(
