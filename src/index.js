6ea40087855212de8a8ce988e20f1adb87d9c456
Cam Jackson <camjackson89@gmail.com>
2018-02-18 11:46:00 +0800

    Don't make concurrent requests when processing multiple records

--- src/index.js
+++ src/index.js
@@ -2,27 +2,29 @@
 
 module.exports = (request, uri, auth, console) => (event, context, callback) => {
   console.log(`event-forwarder lambda received ${event.Records.length} event(s)`);
-  const promises = event.Records.map((record, index) => {
-    console.log(`Forwarding event ${index}`);
-    console.log(`    partitionKey: ${record.kinesis.partitionKey}`);
-    console.log(`    sequenceNumber: ${record.kinesis.sequenceNumber}`);
-    const options = {
-      method: 'POST',
-      uri,
-      body: record.kinesis,
-      json: true,
-      headers: { Authorization: auth },
-    };
+  const sendRecord = (promise, record, index) => (
+    promise.then(() => {
+      console.log(`Forwarding event ${index}`);
+      console.log(`    partitionKey: ${record.kinesis.partitionKey}`);
+      console.log(`    sequenceNumber: ${record.kinesis.sequenceNumber}`);
+      const options = {
+        method: 'POST',
+        uri,
+        body: record.kinesis,
+        json: true,
+        headers: { Authorization: auth },
+      };
 
-    return request(options)
-      .then(() => console.log(`Received success response from API for event ${record.kinesis.sequenceNumber}`))
-      .catch((e) => {
-        console.error(`Error sending event to API. Event: ${record.kinesis.sequenceNumber}, Error: ${e}`);
-        throw e;
-      });
-  });
+      return request(options)
+        .then(() => console.log(`Received success response from API for event ${record.kinesis.sequenceNumber}`))
+        .catch((e) => {
+          console.error(`Error sending event to API. Event: ${record.kinesis.sequenceNumber}, Error: ${e}`);
+          throw e;
+        });
+    })
+  );
 
-  return Promise.all(promises).then(
+  return event.Records.reduce(sendRecord, Promise.resolve()).then(
     () => { callback(null, 'ok'); },
     (err) => { callback(err); }
   );
